ea14e0acf8fb79de14815831cd45b34d5fe5b4ca
Ola Sundell <ola.sundell@gmail.com>
2011-12-17 15:46:25 +0100

    Svara -> Nästa (ladda om) javascript stuff finished.

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,27 +1,38 @@
-$(function() {
-    $("#answerbutton").click(function() {
-        var currentName = $('input[name=choice]:checked').val();
-        var dataString = 'choice='+currentName + '&id='+$("#id").val();
+var nextBirdHandler = function() {
+    window.location = "/random/";
 
-        $.ajax({
-            type: "POST",
-            url: "/random/",
-            data: dataString,
-            success: function(data, textStatus, jqXHR) {
-                $('#'+data).addClass('correct');
-                /*            if (currentName == data) {
-                 alert ("correct")
-                 $('input[name=choice]:checked').parent("li").addClass('correct');
-                 } else {
-                 alert("incorrect");
-                 $('input[name=choice]').each(function(){
-                 if (this.value == data) {
-                 this.parent("li").addClass('correct');
-                 }
-                 })
-                 }*/
-            }
-        });
-        return false;
-    })
+    return false;
+};
+
+var submitAnswerHandler = function() {
+    var currentName = $('input[name=choice]:checked').val();
+    var dataString = 'choice='+currentName + '&id='+$("#id").val();
+
+    $.ajax({
+        type: "POST",
+        url: "/random/",
+        data: dataString,
+        success: function(data, textStatus, jqXHR) {
+            $('#'+data).addClass('correct');
+            $('#answerbutton').text('Nästa');
+            $('#answerbutton').click(nextBirdHandler);
+//            $('#answerbutton').unbind('click');
+//            $('#answerbutton').bind('click', nextBirdHandler);
+            /*            if (currentName == data) {
+             alert ("correct")
+             $('input[name=choice]:checked').parent("li").addClass('correct');
+             } else {
+             alert("incorrect");
+             $('input[name=choice]').each(function(){
+             if (this.value == data) {
+             this.parent("li").addClass('correct');
+             }
+             })
+             }*/
+        }
+    });
+    return false;
+};
+$(document).ready(function() {
+    $("#answerbutton").click(submitAnswerHandler);
 });
